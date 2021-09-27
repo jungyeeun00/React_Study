a5bea04f8d20f2283be7c5cd38e101a4bb1e4859
jungyeeun00 <ekfqlc0103@naver.com>
2021-09-27 14:00:35 +0900

    chapter 13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    )
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Subject title="WEB" sub="world wide web!!"></Subject>
+      <Subject title="React" sub="for UI"></Subject>
+      <TOC></TOC>
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
   );
 }
