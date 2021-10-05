c706622f8eddce70b0340e4e9134fee61a252469
jungyeeun00 <ekfqlc0103@naver.com>
2021-10-06 01:54:48 +0900

    chapter 16,17

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      mode:'read',
+      selected_content_id:2,
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       subject:{title:'WEB', sub:'World Wide Web'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is HyperText'},
@@ -18,13 +21,39 @@ class App extends Component{
     }
   }
   render(){
+    console.log('App render');
+    var _title, _desc=null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }else if(this.state.mode==='read'){
+      var i = 0;
+      while(i<this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id===this.state.selected_content_id){
+          _title=data.title;
+          _desc=data.desc;
+        }
+        i++;
+      }
+    }
+    console.log('render', this);
     return (
       <div className="App">
         <Subject title={this.state.subject.title} 
-        sub={this.state.subject.sub}></Subject>
-        <Subject title="React" sub="for UI"></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        sub={this.state.subject.sub}
+        onChangePage={function () {
+          this.setState({mode:'welcome'})
+        }.bind(this)}
+        ></Subject>
+        <TOC onChangePage={function (id) {
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          })
+        }.bind(this)}
+         data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 class TOC extends Component{
     render(){
+      console.log('TOC render');
         var lists=[];
         var data = this.props.data;
         var i = 0;
         while(i < data.length){
-            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            lists.push(
+            <li key={data[i].id}>
+              <a href={"/content/"+data[i].id}
+              onClick={function (id, e) {
+                e.preventDefault();
+                this.props.onChangePage(id);
+              }.bind(this, data[i].id)}
+              >{data[i].title}</a>
+            </li>);
             i++;
         }
       return(
